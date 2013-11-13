669f82e5e3a386ce06c3ced8ae1d1c8c8e689c1f
Jaitnium <mantosia@calpoly.edu>
2013-11-13 13:44:19 -0800

    boom.sh

---
+++ boom.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+arg1=$1
+arg2=$2
+arg3=$3
+
+export PATH=/home/clupo/mips/bin:${PATH}
+mips-elf-gcc $arg1 -static -msoft-float -o $arg2
+mips-elf-objdump -D $arg2 | gensimcode > $arg3
+./mipsim -d -f $arg3
+
+
+
--- testAdd.s
+++ testAdd.s
@@ -2,5 +2,4 @@
 .globl main
 main:
      addiu $31, $0, 1
-     addiu $31, $0, -1
      jr    $31
