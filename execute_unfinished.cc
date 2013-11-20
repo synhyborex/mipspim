c7dbfddc2cace3130a508a096d1f3fba647fb86a
synhyborex <bbrahmbh@calpoly.edu>
2013-11-19 18:01:04 -0800

    some stats

--- execute_unfinished.cc
+++ execute_unfinished.cc
@@ -6,6 +6,31 @@ Caches caches(0);
 unsigned int jumpTo = 0;
 bool jump_flag = false;
 bool offset_flag = false;
+/*Stats.instrs = 0;
+
+Stats.numMemWrites = 0;
+Stats.numMemReads = 0;
+Stats.numRType = 0;
+Stats.numIType = 0;
+Stats.numJType = 0;
+Stats.numBranches = 0;
+Stats.numRegWrites = 0;
+Stats.numRegReads = 0;
+
+Stats.numForwardBranchesTaken = 0;
+Stats.numForwardBranchesNotTaken = 0;
+Stats.numBackwardBranchesTaken = 0;
+Stats.numBackwardBranchesNotTaken = 0;
+
+Stats.hasUsefulBranchDelaySlot = 0;
+Stats.hasUselessBranchDelaySlot = 0;
+
+Stats.hasUsefulJumpDelaySlot = 0;
+Stats.hasUselessJumpDelaySlot = 0;
+
+Stats.loadHasLoadUseHazard = 0;
+Stats.loadHasLoadUseStall = 0;
+Stats.loadHasNoLoadUseHazard = 0;*/
 
 unsigned int signExtend16to32ui(short i) {
   return static_cast<unsigned int>(static_cast<int>(i));
@@ -23,7 +48,6 @@ void execute() {
   if(jump_flag){ //jump to address
     pc = jumpTo;
     jump_flag = false;
-    //cout << "pc is " << pc << endl;
   }
   else if(offset_flag){
     pc = pc + jumpTo;
@@ -35,30 +59,57 @@ void execute() {
     switch(rg.func) {
     case SP_ADDU: case SP_ADD:
       rf.write(rt.rd, rf[rt.rs] + rf[rt.rt]);
+      stats.numRType++;
+      stats.numRegReads += 2;
+      stats.numRegWrites++;
       break;
     case SP_SUBU: case SP_SUB:
       rf.write(rt.rd, rf[rt.rs] - rf[rt.rt]);
+      stats.numRType++;
+      stats.numRegReads += 2;
+      stats.numRegWrites++;
       break;
     case SP_SLL:
       rf.write(rt.rd, rf[rt.rt] << rt.sa);
+      stats.numRType++;
+      stats.numRegReads++;
+      stats.numRegWrites++;
       break;
     case SP_SRL:
       rf.write(rt.rd, rf[rt.rt] >> rt.sa);
+      stats.numRType++;
+      stats.numRegReads++;
+      stats.numRegWrites++;
       break;
     case SP_SRA:
       rf.write(rt.rd, rf[rt.rt] / (rt.sa*2));
+      stats.numRType++;
+      stats.numRegReads++;
+      stats.numRegWrites++;
       break;
     case SP_AND:
       rf.write(rt.rd, rf[rt.rs] & rf[rt.rt]);
+      stats.numRType++;
+      stats.numRegReads += 2;
+      stats.numRegWrites++;
       break;
     case SP_OR:
       rf.write(rt.rd, rf[rt.rs] | rf[rt.rt]);
+      stats.numRType++;
+      stats.numRegReads += 2;
+      stats.numRegWrites++;
       break;
     case SP_XOR:
       rf.write(rt.rd, rf[rt.rs] ^ rf[rt.rt]);
+      stats.numRType++;
+      stats.numRegReads += 2;
+      stats.numRegWrites++;
       break;
     case SP_NOR:
       rf.write(rt.rd, !(rf[rt.rs] | rf[rt.rt]));
+      stats.numRType++;
+      stats.numRegReads += 2;
+      stats.numRegWrites++;
       break;
     case SP_SLT:
      // cout<< "Rf[rt.rs] is" << rf[rt.rs] <<endl;
@@ -71,15 +122,23 @@ void execute() {
         // cout<<"Determined rs is not less than rt (0)" << endl;
          rf.write(rt.rd,0);
       }
+      stats.numRType++;
+      stats.numRegReads += 2;
+      stats.numRegWrites++;
       break;
     case SP_JR:
       jumpTo = rf[rt.rs];
       jump_flag = true;
+      stats.numRType++;
+      stats.numRegReads++;
       break;
     case SP_JALR:
       jumpTo = rf[rt.rs];
       jump_flag = true;
       rf.write(rt.rd,pc);
+      stats.numRType++;
+      stats.numRegReads++;
+      stats.numRegWrites++;
       break;
     default:
       cout << "Unsupported instruction: ";
@@ -91,33 +150,78 @@ void execute() {
 
   case OP_ADDIU: case OP_ADDI:
     rf.write(ri.rt, rf[ri.rs] + signExtend16to32ui(ri.imm));
+    stats.numIType++;
+    stats.numRegReads++;
+    stats.numRegWrites++;
     break;
   case OP_J:
-    jumpTo = (pc & 0xf0000000) | (rj.target << 2);
+    jumpTo = (pc & 0xF0000000) | (rj.target << 2);
     jump_flag = true;
+    stats.numJType++;
     break;
   case OP_JAL:
-    jumpTo = (pc & 0xf0000000) | (rj.target << 2);
+    jumpTo = (pc & 0xF0000000) | (rj.target << 2);
     jump_flag = true;
     rf.write(31,pc);
+    stats.numJType++;
+    stats.numRegWrites++;
     break;
   case OP_BEQ:
     if(rf[ri.rs] == rf[ri.rt]){
       jumpTo = signExtend16to32ui(ri.imm) << 2;
       offset_flag = true;
     }
+    if(offset_flag){ //branch taken
+      if(jumpTo < pc)
+        stats.numForwardBranchesTaken++;
+      else stats.numBackwardBranchesTaken++;
+    }
+    else{ //branch not taken
+      if(jumpTo < pc)
+        stats.numForwardBranchesNotTaken++;
+      else stats.numBackwardBranchesNotTaken++;
+    }
+    stats.numIType++;
+    stats.numBranches++;
+    stats.numRegReads += 2;
     break;
   case OP_BNE:
     if(rf[ri.rt] != rf[ri.rs]){
       jumpTo = signExtend16to32ui(ri.imm) << 2;
       offset_flag = true;
     }
+    if(offset_flag){ //branch taken
+      if(jumpTo < pc)
+        stats.numForwardBranchesTaken++;
+      else stats.numBackwardBranchesTaken++;
+    }
+    else{ //branch not taken
+      if(jumpTo < pc)
+        stats.numForwardBranchesNotTaken++;
+      else stats.numBackwardBranchesNotTaken++;
+    }
+    stats.numIType++;
+    stats.numBranches++;
+    stats.numRegReads += 2;
     break; 
   case OP_BLEZ:
     if((signed)rf[ri.rs] <= 0){
       jumpTo = signExtend16to32ui(ri.imm) << 2;
       offset_flag = true;
     }
+    if(offset_flag){ //branch taken
+      if(jumpTo < pc)
+        stats.numForwardBranchesTaken++;
+      else stats.numBackwardBranchesTaken++;
+    }
+    else{ //branch not taken
+      if(jumpTo < pc)
+        stats.numForwardBranchesNotTaken++;
+      else stats.numBackwardBranchesNotTaken++;
+    }
+    stats.numIType++;
+    stats.numBranches++;
+    stats.numRegReads++;
     break;
   case OP_SLTI: case OP_SLTIU:
     if((signed)rf[ri.rs] < (signed)signExtend16to32ui(ri.imm)){
@@ -126,38 +230,65 @@ void execute() {
     else {
        rf.write(ri.rt, 0);
     }
+    stats.numIType++;
+    stats.numRegReads++;
+    stats.numRegWrites++;
     break;
   case OP_ORI:
     rf.write(ri.rt, rf[ri.rs] | signExtend16to32ui(ri.imm));
+    stats.numIType++;
+    stats.numRegReads++;
+    stats.numRegWrites++;
     break;
   case OP_SB:
     addr = rf[ri.rs] + signExtend16to32ui(ri.imm);
     caches.access(addr);
     dmem.write(addr, (0xFF & rf[ri.rt]) << 24); //move it to msb
+    stats.numIType++;
+    stats.numMemWrites++;
+    stats.numRegReads++;
     break;
   case OP_LBU:
     addr = rf[ri.rs] + signExtend16to32ui(ri.imm);
     caches.access(addr);
     rf.write(ri.rt, dmem[addr].data_ubyte4(0));
+    stats.numIType++;
+    stats.numMemReads++;
+    stats.numRegReads++;
+    stats.numRegWrites++;
     break;
   case OP_SW:
     addr = rf[ri.rs] + signExtend16to32ui(ri.imm);
     caches.access(addr);
     dmem.write(addr, rf[ri.rt]);
+    stats.numIType++;
+    stats.numMemWrites++;
+    stats.numRegReads++;
     break;
   case OP_LW:
     addr = rf[ri.rs] + signExtend16to32ui(ri.imm);
     caches.access(addr);
     rf.write(ri.rt, dmem[addr]);
+    stats.numIType++;
+    stats.numMemReads++;
+    stats.numRegReads++;
+    stats.numRegWrites++;
     break;
   case OP_LB:
     addr = rf[ri.rs] + signExtend16to32ui(ri.imm);
     caches.access(addr);
     rf.write(ri.rt, (signed)(dmem[addr].data_ubyte4(0)));
+    stats.numIType++;
+    stats.numMemReads++;
+    stats.numRegReads++;
+    stats.numRegWrites++;
     break;
   case OP_LUI:
     rf.write(ri.rt, rf[ri.rt] & 0x0000FFFF); //clear upper bits
     rf.write(ri.rt, rf[ri.rt] | (ri.imm << 16));
+    stats.numIType++;
+    stats.numRegReads++;
+    stats.numRegWrites++;
     break;
   default:
     cout << "Unsupported instruction: ";
